c9eec5df889aaec8c1d63d9cccb20a9f67723821
Phakinthorn Pronmongkolsuk <kinphakinthorn@gmail.com>
2026-02-05 20:32:57 +0700

    Fix unknown endDate null value error

--- src/app/components/CompetitionDetails.tsx
+++ src/app/components/CompetitionDetails.tsx
@@ -14,7 +14,7 @@ const CompetitionDetails = ({competition}: {competition: any}) =>
         <>
             <div className='mx-auto w-fit text-left'>
                 <p className='text-4xl font-bold'>{competition?.name ?? ''}</p>
-                <p className='text-xl'>{competition?.startDate ? dateToRange(competition?.startDate, competition?.endDate) : ''} @{competition.venue ?? ''}</p>
+                <p className='text-xl'>{competition?.startDate && competition?.endDate ? dateToRange(competition?.startDate, competition?.endDate) : ''} @{competition?.venue ?? ''}</p>
                 <Button className='mt-5' color='success' variant='flat' as={Link} href='https://docs.google.com/forms/d/e/1FAIpQLSfizf8EuVAvcKO86AamWJyAEmjXgHHIT08LOvTG3cr6zz3exA/viewform?usp=header'>{t('CompetitionInfo.register')}</Button>
                 <Button className='mt-5 mx-5' color='warning' variant='flat' as={Link} href='#schedule'>{t('CompetitionInfo.schedule')}</Button>
             </div>
