5f255672e3b3457f8cd4c57e6146480f1892071d
Phakinthorn Pronmongkolsuk <kinphakinthorn@gmail.com>
2026-02-05 20:23:41 +0700

    Temporary handle null values in competition rendering

--- src/app/components/CompetitionDetails.tsx
+++ src/app/components/CompetitionDetails.tsx
@@ -13,8 +13,8 @@ const CompetitionDetails = ({competition}: {competition: any}) =>
     return (
         <>
             <div className='mx-auto w-fit text-left'>
-                <p className='text-4xl font-bold'>{competition.name}</p>
-                <p className='text-xl'>{dateToRange(competition.startDate, competition.endDate)} @{competition.venue}</p>
+                <p className='text-4xl font-bold'>{competition.name ?? ''}</p>
+                <p className='text-xl'>{competition.startDate ? dateToRange(competition.startDate, competition.endDate) : ''} @{competition.venue ?? ''}</p>
                 <Button className='mt-5' color='success' variant='flat' as={Link} href='https://docs.google.com/forms/d/e/1FAIpQLSfizf8EuVAvcKO86AamWJyAEmjXgHHIT08LOvTG3cr6zz3exA/viewform?usp=header'>{t('CompetitionInfo.register')}</Button>
                 <Button className='mt-5 mx-5' color='warning' variant='flat' as={Link} href='#schedule'>{t('CompetitionInfo.schedule')}</Button>
             </div>
