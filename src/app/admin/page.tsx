08b68d08b3808bfee998741fdb468f1d0539d82a
kinpkt <phakhinthon@gmail.com>
2026-02-18 13:09:20 +0700

    Add PIN before allowing login with WCA

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import { Card, CardHeader, CardBody, CardFooter, Link, Button, Image } from '@heroui/react';
+import { Card, CardHeader, CardBody, CardFooter, Link, Button, Image, InputOtp, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Form } from '@heroui/react';
 import { signIn, useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Page = () =>
 {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const router = useRouter();
+
+    useEffect(() => 
+    {
+        onOpen();
+    }, []);
+
     return (
         <>
             <Card className='w-[75%] mx-auto'>
@@ -12,6 +22,40 @@ const Page = () =>
                     <Button as={Link} color='primary' variant='flat' onPress={() => signIn('wca')}><Image src='/img/wca.svg' width={30} height={30}/>Login with WCA</Button>
                 </CardBody>
             </Card>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className='flex flex-col gap-1'>
+                                Passcode Required
+                            </ModalHeader>
+                            <ModalBody className='pb-6'>
+                                <p className='text-sm text-default-500 mb-4 text-center'>
+                                    Please enter your 4-digit passcode below.
+                                </p>
+                                <Form className='mx-auto' onSubmit={
+                                    (e) =>
+                                    {
+                                        e.preventDefault();
+                                        const formData = new FormData(e.currentTarget);
+                                        const data = formData.get('passcode')?.toString() || '';
+
+                                        if (data === process.env.NEXT_PUBLIC_ADMIN_PASSCODE?.toString())
+                                            onClose();
+                                        else
+                                            router.push('/');
+                                    }
+                                }>
+                                    <InputOtp errorMessage='Invalid Passcode' name='passcode' length={4} placeholder='Enter Passcode' isRequired/>
+                                    <Button type='submit' color='primary' className='mt-4 w-full'>
+                                        Verify
+                                    </Button>
+                                </Form>
+                            </ModalBody>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </>
     );
 }
